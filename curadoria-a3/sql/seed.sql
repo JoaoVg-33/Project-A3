bf5c05e51f4b7fccaacf96d30c410c5a9456acca
João Victor Gabriel <825149732@ulife.com.br>
2025-11-24 17:43:59 -0300

    Update seed.sql

--- curadoria-a3/sql/seed.sql
+++ curadoria-a3/sql/seed.sql
@@ -1,4 +1,11 @@
 USE curadoria_a3;
 -- Atenção: inserir hash de senha na prática. Aqui usamos senha simples 'admin' como exemplo.
-INSERT INTO users (name, age, username, password_hash, role) VALUES
-('Admin Inicial', 30, 'admin', '$2a$10$7Qy/ExampleHashReplaceMe1234567890abcdefg', 'ADMIN');
+INSERT INTO users (username, name, age, role, password_hash, active)
+VALUES (
+  'admin',
+  'Administrador do Sistema',
+  30,
+  'ADMIN',
+  '$2a$10$0GZ77O8YoX/aBXtHk9KmeOBnDowTmxLxzLuJoVR31Wd5p5o9x5Eze', 
+  1
+)
